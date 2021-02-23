e14658e8cb4cb9b08a2273655df808f31bf6d211
Abdulazeez Shittu <41093124+abdulazeez001@users.noreply.github.com>
2021-02-23 10:46:13 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user:"postgres",
-    password:"olawale 1",
+    password:"********",
     host:"localhost",
     port:5432,
     database:"teamwork"
